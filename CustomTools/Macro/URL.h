08be0b43b5fa0d35af1954c46a9aa07730a41319
gaosy <gaosy@kmelearning.com>
2021-02-08 17:00:45 +0800

    删除没用的url

--- CustomTools/Macro/URL.h
+++ CustomTools/Macro/URL.h
@@ -14,28 +14,7 @@
 #define COMBINE_STRINGS(A, B) [NSString stringWithFormat:@"%@%@", A, B]
 #define CREATE_URL(PATH) COMBINE_STRINGS(BASE_URL, PATH)
 
-#define LIVE_CHANNEL_CREATE CREATE_URL(@"/live/student/liveChannel/save")
-#define LIVE_CHANNEL_CANCEL CREATE_URL(@"/live/student/liveChannel/update/cancel")
-#define LIVE_CHANNEL_STATUS CREATE_URL(@"/live/student/liveChannel/get")
-#define LIVE_CREATE CREATE_URL(@"/live/student/liveActivity/save")
-#define LIVE_UPDATE CREATE_URL(@"/live/student/liveActivity/update")
-#define LIVE_CANCEL CREATE_URL(@"/live/student/liveActivity/update/cancel")
-//#define LIVE_INFORMATION CREATE_URL(@"/live/student/liveActivity/get")
-#define LIVE_ONLINE_NUMBER CREATE_URL(@"/live/student/liveActivity/get/onlineNum")
-//#define LIVE_PAUSE CREATE_URL(@"/live/student/liveActivity/pause")
-//#define LIVE_RESUME CREATE_URL(@"/live/student/liveActivity/resume")
-#define COMMON_LIKE_COUNT CREATE_URL(@"/common/student/commonAdmire/get")
-#define COMMON_LIKE_ADD CREATE_URL(@"/common/student/commonAdmire/save")
-#define COMMON_LIKE_REDUCE CREATE_URL(@"/common/student/commonAdmire/delete")
-
-#define VIDEO_AUTH CREATE_URL(@"/aliyun/student/video/address/auth/get")
 #define VIDEO_AUTH_REFRESH CREATE_URL(@"/aliyun/student/video/address/auth/refresh/get")
-//#define VIDEO_INFORMATION CREATE_URL(@"/aliyun/student/video/play/url/get")
-#define VIDEO_CLASSIFY_LIST CREATE_URL(@"/video/student/videoClassify/classify/list")
-#define VIDEO_CLASSIFY_CHANNEL_LIST CREATE_URL(@"/video/student/videoClassify/channel/by/classify/list")
-#define VIDEO_CLASSIFY_CHANNEL_SEARCH CREATE_URL(@"/video/student/videoClassify/interested/channel/list")
-#define VIDEO_PUBLISH CREATE_URL(@"/video/student/video/save")
-#define IMAGE_AUTH CREATE_URL(@"/aliyun/student/image/address/auth/get")
 
 #define USER_LOGIN CREATE_URL(@"/auth/oauth/token")
 
