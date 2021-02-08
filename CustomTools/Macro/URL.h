929797d535b4ff99e563162545e28a8e28621fae
gaosy <gaosy@kmelearning.com>
2021-02-08 16:35:13 +0800

    修改baseurl

--- CustomTools/Macro/URL.h
+++ CustomTools/Macro/URL.h
@@ -9,7 +9,8 @@
 #ifndef Network_h
 #define Network_h
 
-#define BASE_URL @"http://192.168.1.98:8301"
+#define BASE_URL @""
+//#define BASE_URL @""
 #define COMBINE_STRINGS(A, B) [NSString stringWithFormat:@"%@%@", A, B]
 #define CREATE_URL(PATH) COMBINE_STRINGS(BASE_URL, PATH)
 
@@ -24,6 +25,8 @@
 //#define LIVE_PAUSE CREATE_URL(@"/live/student/liveActivity/pause")
 //#define LIVE_RESUME CREATE_URL(@"/live/student/liveActivity/resume")
 #define COMMON_LIKE_COUNT CREATE_URL(@"/common/student/commonAdmire/get")
+#define COMMON_LIKE_ADD CREATE_URL(@"/common/student/commonAdmire/save")
+#define COMMON_LIKE_REDUCE CREATE_URL(@"/common/student/commonAdmire/delete")
 
 #define VIDEO_AUTH CREATE_URL(@"/aliyun/student/video/address/auth/get")
 #define VIDEO_AUTH_REFRESH CREATE_URL(@"/aliyun/student/video/address/auth/refresh/get")
@@ -37,8 +40,8 @@
 #define USER_LOGIN CREATE_URL(@"/auth/oauth/token")
 
 //url后面加websocket，否则报错Error Domain=SRWebSocketErrorDomain Code=2133 "Invalid Sec-WebSocket-Accept response"
-#define SOCKET_SERVER_URL @"ws://192.168.1.21:31052/liveRoom/websocket"
+#define SOCKET_SERVER_URL CREATE_URL(@"/common/liveRoom/websocket")
 #define SOCKET_SEND_URL(liveId) COMBINE_STRINGS(@"/formApp/student/accept.", liveId)
-#define SOCKET_SUBSCRIBE_URL(channelId) COMBINE_STRINGS(@"/topic/222.", channelId)
+#define SOCKET_SUBSCRIBE_URL(channelId) COMBINE_STRINGS(@"/topic/getResponse.", channelId)
 
 #endif /* Network_h */
